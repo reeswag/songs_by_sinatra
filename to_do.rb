8284fa9ab07ac4119dd061bae3345527b0ec66ff
reeswag <reeswag@gmail.com>
2020-01-28 13:25:57 +0000

    inital commit

---
+++ to_do.rb
@@ -0,0 +1,18 @@
+----
+https://toolbelt.heroku.com/ 
+install heroku toolbelt
+
+console:
+
+heroku create Sinatra_by_Reeswag
+
+gem install bundler
+
+create new file within project root : Gemfile (no extension) # done
+
+bundle install --without production
+
+git push heroku master 
+
+heroku open
+
