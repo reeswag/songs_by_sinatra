feb4de0a6b02d736ad38544021721beb492b69cc
reeswag <reeswag@gmail.com>
2020-01-30 20:20:31 +0000

    added database configuration

--- main.rb
+++ main.rb
@@ -12,7 +12,7 @@ end
 configure :development do
     DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
-
+  
 configure :production do
     DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
